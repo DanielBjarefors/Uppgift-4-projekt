a7349a6d3c6f7b8efb40a25b53f879d371a7de29
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-03-03 15:20:42 +0100

    ts logic

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 Vue.createApp({
     data: function () {
         return {
-            workoutCounter: 3,
-            reps: 0,
-            rows: [{
-                    weight: "",
-                    date: new Date().toISOString().slice(0, 10),
-                    sets: ""
-                }]
+            header: 'Shopping List App',
+            newItem: [],
+            startWeight: [],
+            reps: [{ set1: 0 }, { set2: 0 }, { set3: 0 }, { set4: 0 }, { set5: 0 }],
+            newWeight: '',
+            items: []
         };
     },
     methods: {
-        setStartingWeight: function () {
-            this.rows[0].weight = this.startWeight;
+        saveItem: function () {
+            var copy1 = Object.assign({}, this.startWeight);
+            var copy2 = Object.assign({}, this.reps);
+            this.items.push({
+                startWeight: copy1,
+                reps: copy2
+            }),
+                this.startWeight.push(this.reps.set1 > 5 ? this.startWeight[0] + 5 : this.startWeight[0], this.reps.set2 > 5 ? this.startWeight[1] + 5 : this.startWeight[1], this.reps.set3 > 5 ? this.startWeight[2] + 5 : this.startWeight[2], this.reps.set4 > 5 ? this.startWeight[3] + 5 : this.startWeight[3], this.reps.set5 > 5 ? this.startWeight[4] + 5 : this.startWeight[4]);
+            this.startWeight.splice(0, 5);
+            this.reps.splice(0, 5);
         },
-        endWorkout: function () {
-            // this.rows.weight = this.startingWeight
-            this.workoutCounter++;
-            // this.rows[0].weight = this.rows[0].weight+5;
-            var newWeight = 0;
-            if (this.reps > 5) {
-                newWeight = this.rows[0].weight + 5;
-            }
-            this.rows.push({
-                weight: newWeight,
-                date: new Date().toISOString().slice(0, 10),
-                sets: 5
-            });
+        setStartingWeight: function () {
+            this.startWeight.push(this.newWeight, this.newWeight, this.newWeight, this.newWeight, this.newWeight);
+            this.newItem = "";
         }
     }
 }).mount('main');
+// Vue.createApp({
+//     data() {
+//         return {
+//             workoutCounter: 1,
+//             weight:[101,102,103,104,105],
+//                 set1:0,set2:0,set3:0,set4:0,set5:0,
+//                 date: new Date().toISOString().slice(0,10)  ,
+//             rows: [{
+//                 weight:[101,102,103,104,105],
+//                 rep:[6,7,5,4,3],
+//                 date: new Date().toISOString().slice(0,10)                                              
+//             }],
+//         };
+//     },
+//     methods: {
+//         setStartingWeight(){
+//             this.rows[0].weight= this.startWeight
+//         },
+//         endWorkout(){
+//             // this.rows.weight = this.startingWeight
+//             this.rows.push({
+//                 weight: [101,102,103,104,107],
+//                 rep:  [this.set1,this.set2,this.set3,this.set4,this.set5],
+//                 workoutCounter: this.workoutCounter +1,
+//                 date: new Date().toISOString().slice(0,10)
+//             });
+//         },
+//     }
+// }).mount('main');
 //# sourceMappingURL=script.js.map
