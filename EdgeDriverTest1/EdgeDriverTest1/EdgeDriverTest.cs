db0e1cfeeb4723e4b34d6e58206f01c9b7e7f83f
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-03-11 19:46:21 +0100

    tests and letters

--- EdgeDriverTest1/EdgeDriverTest1/EdgeDriverTest.cs
+++ EdgeDriverTest1/EdgeDriverTest1/EdgeDriverTest.cs
@@ -15,85 +15,33 @@ namespace SeleniumExample
     public class EdgeDriverTest
     {
         private const string edgeDriverDirectory = @"C:\C#\Frontend med Jakob Kallin\edgedriver_win64 (1)";
-        private const string BenchPress = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/BenchPress.html";
-        private const string Deadlift = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/Deadlift.html";
-        private const string Squat = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/Squat.html";
+        private const string benchPress = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/benchPress.html";
+        private const string deadlift = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/deadlift.html";
+        private const string squat = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/squat.html";
         private const string index = "file:///C:/C%23/Frontend%20med%20Jakob%20Kallin/Uppgift-4-projekt/index.html";
 
-        private EdgeDriver browserBench;
-        private EdgeDriver browserDeadlift;
-        private EdgeDriver browserSquat;
-        private EdgeDriver browserindex;
+        private EdgeDriver browser;
+
 
         // This is run before each test.
         [TestInitialize]
         public void EdgeDriverInitialize()
         {
-            browserBench = new EdgeDriver(edgeDriverDirectory);
-            browserBench.Url = BenchPress;
-
-            browserDeadlift = new EdgeDriver(edgeDriverDirectory);
-            browserDeadlift.Url = Deadlift;
-
-            browserSquat = new EdgeDriver(edgeDriverDirectory);
-            browserSquat.Url = Squat;
-
-            browserindex = new EdgeDriver(edgeDriverDirectory);
-            browserindex.Url = index;
+            browser = new EdgeDriver(edgeDriverDirectory);
+            browser.Url = index;
         }
+       
+        
         [TestMethod]
-        public void TestSquatPage()
+        public void TestBenchAndIndexPage()
         {
+            browser.Navigate().GoToUrl(benchPress);
+
             // Find the search input at the top of the page.
-            var btn = browserBench.FindElementsByCssSelector("button");
-            var input = browserBench.FindElementsByCssSelector("input");
-            var weight = browserBench.FindElementsByCssSelector("td");
+            var btn = browser.FindElementsByCssSelector("button");
+            var input = browser.FindElementsByCssSelector("input");
+            var weight = browser.FindElementsByCssSelector("td");
 
-            input[0].Clear();
-            input[0].SendKeys("120");
-            btn[1].Click();
-
-            input[1].Clear();
-            input[1].SendKeys("6");
-            input[2].Clear();
-            input[2].SendKeys("6");
-            btn[2].Click();
-
-            Assert.AreEqual("125", weight[1].Text);
-            Assert.AreEqual("125", weight[2].Text);
-            btn[0].Click();
-        }
-
-        [TestMethod]
-        public void TestDeadliftPage()
-        {
-            // Find the search input at the top of the page.
-            var btn = browserDeadlift.FindElementsByCssSelector("button");
-            var input = browserDeadlift.FindElementsByCssSelector("input");
-            var weight = browserDeadlift.FindElementsByCssSelector("td");
-
-            input[0].Clear();
-            input[0].SendKeys("120");
-            btn[1].Click();
-
-            input[1].Clear();
-            input[1].SendKeys("6");
-            input[2].Clear();
-            input[2].SendKeys("6");
-            btn[2].Click();
-
-            Assert.AreEqual("125", weight[1].Text);
-            Assert.AreEqual("125", weight[2].Text);
-            btn[0].Click();
-        }
-
-        [TestMethod]
-        public void TestBenchPage()
-        {
-            // Find the search input at the top of the page.
-            var btn = browserBench.FindElementsByCssSelector("button");
-            var input = browserBench.FindElementsByCssSelector("input");
-            var weight = browserBench.FindElementsByCssSelector("td");
 
             input[0].Clear();
             input[0].SendKeys("90");
@@ -104,15 +52,60 @@ namespace SeleniumExample
             btn[2].Click();
 
             Assert.AreEqual("95", weight[1].Text);
-            btn[0].Click();
+            
+            browser.Navigate().GoToUrl(index);
+            var total = browser.FindElementsByCssSelector("th");
+            Assert.IsTrue(total[4].Text == "455");
         }
-       
+        [TestMethod]
+        public void TestDeadliftPage()
+        {
+            browser.Navigate().GoToUrl(deadlift);
 
-        
+            // Find the search input at the top of the page.
+            var btn = browser.FindElementsByCssSelector("button");
+            var input = browser.FindElementsByCssSelector("input");
+            var weight = browser.FindElementsByCssSelector("td");
+
+            input[0].Clear();
+            input[0].SendKeys("120");
+            btn[1].Click();
+
+            input[1].Clear();
+            input[1].SendKeys("6");
+            btn[2].Click();
+
+            Assert.AreEqual("125", weight[1].Text);
+            //btn[0].Click();
+        }
+        [TestMethod]
+        public void TestSquatPage()
+        {
+            browser.Navigate().GoToUrl(squat);
+            // Find the search input at the top of the page.
+            var btn = browser.FindElementsByCssSelector("button");
+            var input = browser.FindElementsByCssSelector("input");
+            var weight = browser.FindElementsByCssSelector("td");
+
+            input[0].Clear();
+            input[0].SendKeys("120");
+            btn[1].Click();
+
+            input[1].Clear();
+            input[1].SendKeys("6");
+            input[2].Clear();
+            input[2].SendKeys("6");
+            btn[2].Click();
+
+            Assert.AreEqual("125", weight[1].Text);
+            Assert.AreEqual("125", weight[4].Text);
+            //btn[0].Click();
+        }
+
         [TestCleanup]
         public void EdgeDriverCleanup()
         {
-            browserBench.Quit();
+            browser.Quit();
         }
     }
 }
