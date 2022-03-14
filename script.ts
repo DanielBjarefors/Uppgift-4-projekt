f34ef06859990125ca8984755ec811727ede635f
DanielBjarefors <90314195+DanielBjarefors@users.noreply.github.com>
2022-03-14 12:59:46 +0100

    text

--- script.ts
+++ script.ts
@@ -77,7 +77,7 @@ Vue.createApp({
             //hide "set starting weight" when it´s set
             this.toggle = !this.toggle;
         },
-        //check local storage for data
+        //check local storage for data and load 
         setUnits() {
             let check = JSON.parse(window.localStorage.getItem('completedWorkouts' + this.id));
             if (check !== null) {
@@ -86,7 +86,7 @@ Vue.createApp({
                 this.workoutNr = JSON.parse(window.localStorage.getItem('workoutNr' + this.id));
                 this.total = JSON.parse(window.localStorage.getItem('total' + this.id));
             }
-            //hide "set starting weight" if loading from starage
+            //hide "set starting weight" if loading from storage
             if (check !== null) {
                 this.toggle = true;
             }
